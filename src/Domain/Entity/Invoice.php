4c66fc843df133d53155690dd698791ab48f5f31
Sebastiaan de Jonge <mail@sebastiaandejonge.com>
2019-01-03 19:46:58 +0200

    Add invoice entity

---
+++ src/Domain/Entity/Invoice.php
@@ -0,0 +1,79 @@
+<?php
+namespace Sebs\Tca\Domain\Entity;
+
+use DateTime;
+
+/**
+ * @package Sebs\Tca\Domain\Entity
+ */
+class Invoice extends AbstractEntity
+{
+    /**
+     * @var Order
+     */
+    protected $order;
+
+    /**
+     * @var DateTime
+     */
+    protected $invoiceDate;
+
+    /**
+     * @var float
+     */
+    protected $total;
+
+    /**
+     * @return Order
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * @param Order $order
+     * @return self
+     */
+    public function setOrder($order)
+    {
+        $this->order = $order;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getInvoiceDate()
+    {
+        return $this->invoiceDate;
+    }
+
+    /**
+     * @param DateTime $invoiceDate
+     * @return self
+     */
+    public function setInvoiceDate($invoiceDate)
+    {
+        $this->invoiceDate = $invoiceDate;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * @param float $total
+     * @return self
+     */
+    public function setTotal($total)
+    {
+        $this->total = $total;
+        return $this;
+    }
+}
