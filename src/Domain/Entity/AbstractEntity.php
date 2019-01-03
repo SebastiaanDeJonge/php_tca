44259439e341e82f1ef59b4ca9b24bb8c42eb284
Sebastiaan de Jonge <mail@sebastiaandejonge.com>
2019-01-03 19:40:47 +0200

    Add abstract entity

---
+++ src/Domain/Entity/AbstractEntity.php
@@ -0,0 +1,31 @@
+<?php
+namespace Sebs\Tca\Domain\Entity;
+
+/**
+ * @package Sebs\Tca\Domain\Entity
+ */
+abstract class AbstractEntity
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+}
