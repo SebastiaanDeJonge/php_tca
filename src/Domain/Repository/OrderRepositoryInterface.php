339009bb62a919c45255973d76d0c742a4c9f799
Sebastiaan de Jonge <mail@sebastiaandejonge.com>
2019-01-03 19:58:41 +0200

    Add method to retrieve uninvoiced orders

--- src/Domain/Repository/OrderRepositoryInterface.php
+++ src/Domain/Repository/OrderRepositoryInterface.php
@@ -1,10 +1,15 @@
 <?php
 namespace Sebs\Tca\Domain\Repository;
 
+use Sebs\Tca\Domain\Entity\Order;
+
 /**
  * @package Sebs\Tca\Domain\Repository
  */
 interface OrderRepositoryInterface extends RepositoryInterface
 {
-
+    /**
+     * @return Order[]
+     */
+    public function getUninvoicedOrders();
 }
