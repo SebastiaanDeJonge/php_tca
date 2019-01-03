1f8a35f25a9305b3ae6fe923d055e14e0212fd38
Sebastiaan de Jonge <mail@sebastiaandejonge.com>
2019-01-03 19:45:27 +0200

    Add order entity

---
+++ src/Domain/Entity/Order.php
@@ -0,0 +1,100 @@
+<?php
+namespace Sebs\Tca\Domain\Entity;
+
+/**
+ * @package Sebs\Tca\Domain\Entity
+ */
+class Order extends AbstractEntity
+{
+    /**
+     * @var Customer
+     */
+    protected $customer;
+
+    /**
+     * @var string
+     */
+    protected $orderNumber;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @var float
+     */
+    protected $total;
+
+    /**
+     * @return Customer
+     */
+    public function getCustomer()
+    {
+        return $this->customer;
+    }
+
+    /**
+     * @param Customer $customer
+     * @return self
+     */
+    public function setCustomer($customer)
+    {
+        $this->customer = $customer;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrderNumber()
+    {
+        return $this->orderNumber;
+    }
+
+    /**
+     * @param string $orderNumber
+     * @return self
+     */
+    public function setOrderNumber($orderNumber)
+    {
+        $this->orderNumber = $orderNumber;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return self
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * @param float $total
+     * @return self
+     */
+    public function setTotal($total)
+    {
+        $this->total = $total;
+        return $this;
+    }
+}
