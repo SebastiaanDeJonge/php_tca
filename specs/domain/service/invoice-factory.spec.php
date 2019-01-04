74d71771c12be31e242261757f46fe16a676f52f
Sebastiaan de Jonge <mail@sebastiaandejonge.com>
2019-01-04 21:49:52 +0200

    Add additional behavioral tests for invoice factory

--- specs/domain/service/invoice-factory.spec.php
+++ specs/domain/service/invoice-factory.spec.php
@@ -10,7 +10,29 @@ describe('InvoiceFactory', function() {
             $factory = new InvoiceFactory();
             $invoice = $factory->createFromOrder($order);
 
-            assert($invoice instanceof Invoice, 'instance of Invoice');
+            assert($invoice instanceof Invoice, 'should be an instance of Invoice');
+        });
+
+        it('should set the total of the invoice', function() {
+            $total = 2142356;
+            $order = new Order();
+            $order->setTotal($total);
+            $factory = new InvoiceFactory();
+            $invoice = $factory->createFromOrder($order);
+
+            assert($invoice->getTotal() === $total, 'should associate the Order to the invoice');
+        });
+
+        it('should set the date of the invoice', function() {
+            $dateFormat = 'Y-m-d';
+            $order = new Order();
+            $factory = new InvoiceFactory();
+            $invoice = $factory->createFromOrder($order);
+
+            assert(
+                $invoice->getInvoiceDate()->format($dateFormat) === (new DateTime())->format($dateFormat),
+                'should set the date of the invoice'
+            );
         });
     });
 });
